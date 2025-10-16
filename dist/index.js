12fbd3e462d43992c6b0ccc14dace6d18d3fda3c
Bohdan <booking.bogdan@gmail.com>
2025-10-16 13:59:25 +0300

    configured typescript and created tsconfig.json

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = 'Іван';
+let age = 25;
+let isStudent = true;
+console.log(`Ім'я: ${name}, Вік: ${age}, Студент: ${isStudent}`);
